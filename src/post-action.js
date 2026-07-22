6a87a6156bb2160d42b2bf47f506b7ecdc84951c
Trijito Santoso <tsantoso@veracode.com>
2026-07-22 15:20:53 -0500

    added app to the report file path

--- src/post-action.js
+++ src/post-action.js
@@ -9,7 +9,7 @@ async function post() {
     let artifactFiles = [];
 
     const reportFilename = 'sca-fix-report.md';
-    const artifactFilePath = path.join(workspaceDir, 'source-code', reportFilename);
+    const artifactFilePath = path.join(workspaceDir, 'source-code', 'app', reportFilename);
     if (fs.existsSync(artifactFilePath)) {
         artifactFiles.push(artifactFilePath);
     } else {
